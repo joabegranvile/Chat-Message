018c372be85e304905c87124c6ec150c0903fe30
Joabe <joabe.granvile@yahoo.com>
2024-12-28 01:30:22 -0400

    add server delivery

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+// open db
+const db = await open({
+  filename : 'chat.db',
+  driver: sqlite3.Database,
+});
+
+// create our messages
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  client_offset TEXT UNIQUE,
+  context TEXT,
+  );
+`);
 
 const app = express();
 const PORT = 3000;
@@ -20,7 +37,13 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', async (msg) => {
+    let result;
+    try {
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+    } catch (e) {
+      return;
+    }
     io.emit('chat message', msg);
   });
 });
