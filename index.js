ca4ac500f3e708a226706ae086ea529208c71c4d
Joabe <joabe.granvile@yahoo.com>
2024-12-28 01:46:56 -0400

    client delivery and add condition to not duplicate msg

--- index.js
+++ index.js
@@ -37,15 +37,18 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', async (socket) => {
-  socket.on('chat message', async (msg) => {
+  socket.on('chat message', async (msg, clientOffset, callback) => {
     let result;
     try {
-      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg, clientOffset);
     } catch (e) {
+      if (e.errno === 19) callback();
+
       // TODO handle the failure
       return;
     }
     io.emit('chat message', msg, result.lastID);
+    callback();
   });
 
   if (!socket.recovered) {
